4416e07a15c64bf03460c34faf8055c8346677d7
alejandrolue25 <00235525@uca.edu.sv>
2025-06-08 11:10:29 -0600

    Obstacles .h files

---
+++ headers/fish.h
@@ -0,0 +1,60 @@
+#ifndef FISH_H_INCLUDED
+#define FISH_CURSOR_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/move_cursor.h"
+#include "../headers/submarine.h"
+
+typedef struct
+{
+    int x, y;
+} Fish;
+
+// Pinta la forma del pescado
+void PaintFish(Fish *fish)
+{
+    gotoxy(fish->x, fish->y);
+    printf("<0)))><");
+}
+
+// Borra la forma del pescado
+void DeleteFish(Fish *fish)
+{
+    gotoxy(fish->x, fish->y);
+    printf("        ");
+}
+
+void MoveFish(Fish *fish)
+{
+    gotoxy(fish->x, fish->y);
+    printf("        ");
+    /*Se asigna una posición inicial del pescado en x, y se genera una posición aleatoria en y.
+    La posición del pescado va decreciendo en x, hasta llegar a la posición 5 en x,
+    y luego se vuelve a ejecutar el mismo proceso.*/
+    fish->x--;
+    if (fish->x <= 5)
+    {
+        fish->x = 110;
+        fish->y = rand() % 20 + 3;
+    }
+    PaintFish(fish);
+}
+
+/*Se declara el espacio que ocupa el submarino en "x" y "y", si la posición de "x" y "y"
+coinciden, se detecta un impacto, se reduce la salud del submarino y los obstáculos vuelven a su posición
+inicial.*/
+void collision_fish(Fish *fish, Submarine *submarine)
+{
+    if (fish->x >= submarine->x && fish->x <= submarine->x + 11 && fish->y >= submarine->y && fish->y <= submarine->y + 3)
+    {
+        submarine->heart--;
+        DestroySubmarine(submarine);
+        DeleteFish(fish);
+        PaintSubmarine(submarine);
+        PaintHearts(submarine);
+        fish->x = 110;
+        fish->y = rand() % 20 + 3;
+    }
+}
+#endif
---
+++ headers/mines.h
@@ -0,0 +1,42 @@
+#ifndef MINES_H_INCLUDED
+#define MINES_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/move_cursor.h"
+
+
+typedef struct
+{
+    int x, y;
+} Mines;
+
+
+void PaintMines(Mines *mines)
+{
+    gotoxy(mines->x, mines->y);
+    printf(" %c%c%c%c ", 46, 45, 45, 46);
+    gotoxy(mines->x, mines->y + 1);
+    printf("%c %c%c %c", 47, 88, 88, 92);
+    gotoxy(mines->x, mines->y + 2);
+    printf("%c%c%c%c%c%c", 96, 45, 45, 45, 45, 39);
+}
+
+void MoveMines(Mines *mines)
+{
+    gotoxy(mines->x, mines->y);
+    printf("      ");
+    gotoxy(mines->x, mines->y + 1);
+    printf("      ");
+    gotoxy(mines->x, mines->y + 2);
+    printf("      ");
+    mines->x--;
+    if (mines->x <= 5)
+    {
+        mines->x = 110;
+        mines->y;
+    }
+    PaintMines(mines);
+}
+
+#endif
