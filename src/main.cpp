f02893908bf0904905f9b21e6c529b76ec1cbd44
Javier-Mangandi <00204125@uca.edu.sv>
2025-06-09 21:08:30 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,8 @@ int main()
         Sleep(5);
     } while (submarine.lifes > 0);
 
-    //Holiwis i love u so much, muacks, muacks
+    //Holiwis i love u so much, muacks, muacks (att.Yukio)
+    //mondongo
 
     return 0;
 }
