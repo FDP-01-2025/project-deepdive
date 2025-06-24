6408399d567f0bb662f5484658727131bde088a5
Renemoya26 <00090925@uca.edu.sv>
2025-06-23 21:18:51 -0600

    Llamando funciones a level4

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include "src/headers/menuprincipal.h"
+//#include "src/headers/menuprincipal.h"
 #include "src/headers/hide_cursor.h"
+#include "src/levels/level4.h"
 using namespace std;
 int main()
 {
     HideCursor();
-    runMenu(); // Llama al menú y controla toda su ejecución
+    //runMenu(); // Llama al menú y controla toda su ejecución
+    InitGameLevel4(); // Inicializa el nivel 4 del juego
+    GameLoopLevel4(); // Inicia el bucle del juego para el nivel 4
+
     return 0;
 }
