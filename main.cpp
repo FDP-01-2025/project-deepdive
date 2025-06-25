5baa3c9f951eae249b12d1338d9df2e24936e48a
Javier-Mangandi <00204125@uca.edu.sv>
2025-06-25 11:15:15 -0600

    make pull

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "src/headers/menuprincipal.h"
 #include "src/headers/hide_cursor.h"
+#include "src/levels/level3.h"
 using namespace std;
 int main()
 {
     HideCursor();
-    runMenu(); // Llama al menú y controla toda su ejecución
+    //runMenu(); // Llama al menú y controla toda su ejecución
+    InitGameLevel3();
+    GameLoopLevel3();
+
+
     return 0;
 }
